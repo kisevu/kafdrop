b0a3d0c08539170fb685fb3d43bf0e2fd8f9ce85
ameda-kevin <amedakevin@gmail.com>
2024-05-23 15:42:05 +0300

    push and pull messages from an explicitly specified partition

--- src/main/java/com/ameda/kevin/kafka/consumer/MessageConsumer.java
+++ src/main/java/com/ameda/kevin/kafka/consumer/MessageConsumer.java
@@ -9,6 +9,7 @@ package com.ameda.kevin.kafka.consumer;/*
 import com.ameda.kevin.kafka.producer.dto.Worker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +17,16 @@ import org.springframework.stereotype.Service;
 public class MessageConsumer {
 
 
-    @KafkaListener(topics = "kevin",groupId = "ameda-group-2024")
+//    @KafkaListener(topics = "kevin",groupId = "ameda-group-2024")
+//    public void consume(Worker worker){
+//        log.info("Email:{} and Contact number:{}",worker.getEmail(),worker.getContactNo());
+//    }
+    //same way we can send data to a particular partition by specifying,
+    //the same way we can consume from a specified consumer as well
+    //Below is the implementation
+
+    @KafkaListener(topics = "kevin",groupId = "ameda-group-2024",
+            topicPartitions ={@TopicPartition(topic="kevin",partitions =  {"3"})})
     public void consume(Worker worker){
         log.info("Email:{} and Contact number:{}",worker.getEmail(),worker.getContactNo());
     }
--- src/main/java/com/ameda/kevin/kafka/producer/config/KafkaConfig.java
+++ src/main/java/com/ameda/kevin/kafka/producer/config/KafkaConfig.java
@@ -5,9 +5,20 @@ package com.ameda.kevin.kafka.producer.config;/*
 *
 */
 
+import com.fasterxml.jackson.databind.JsonSerializer;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
@@ -17,4 +28,30 @@ public class KafkaConfig {
     public NewTopic createTopic(){
         return new NewTopic("kevin",5,(short) 1);
     }
+
+    //for ssl provide certificate here
+//    @Bean
+//    public Map<String,Object>  producerConfig(){
+//        Map<String,Object>  props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+//                "localhost:9092");
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+//                StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+//                JsonSerializer.class);
+//        return props;
+//    }
+//
+//    @Bean
+//    public ProducerFactory<String,Object> producerFactory(){
+//        return new DefaultKafkaProducerFactory<>(producerConfig());
+//    }
+
+//    @Bean
+//    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String,Object>> kafkaListenerContainerFactory(){
+//        ConcurrentKafkaListenerContainerFactory<String,Object> factory
+//                = new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+//    }
 }
